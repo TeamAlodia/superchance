055a1a0fc5b37d6efb381d6b5ad19831bda085c4
Justin <justin.m.kincaid@gmail.com>
2016-10-03 10:22:16 -0700

    add basic find functions to player.java

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -37,6 +37,16 @@ public class Player {
     password = _password;
   }
 
+  // Find Functions
+  public static List<Player> all() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM players";
+      List<Player> allPlayers = con.createQuery(sql)
+      .executeAndFetch(Player.class);
+      return allPlayers;
+    }
+  }
+
   // Database Functions
   public void save() {
       try(Connection con = DB.sql2o.open()) {
@@ -50,34 +60,42 @@ public class Player {
       }
     }
 
-    public void update() {
-      try(Connection con = DB.sql2o.open()) {
-        String sql = "UPDATE players SET name = :name, created = :created, password = :password WHERE id = :id";
-        con.createQuery(sql)
-          .addParameter("name", name)
-          .addParameter("created", created)
-          .addParameter("password", password)
-          .addParameter("id", id)
-          .executeUpdate();
-      }
+  public void update() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "UPDATE players SET name = :name, created = :created, password = :password WHERE id = :id";
+      con.createQuery(sql)
+        .addParameter("name", name)
+        .addParameter("created", created)
+        .addParameter("password", password)
+        .addParameter("id", id)
+        .executeUpdate();
     }
+  }
 
-    public void delete() {
-      try(Connection con = DB.sql2o.open()) {
-        String sql = "DELETE FROM players WHERE id = :id";
-        con.createQuery(sql)
-          .addParameter("id", id)
-          .executeUpdate();
-        String sql3 = "DELETE FROM cards_monsters_players where player_id = :id";
-        con.createQuery(sql3)
-          .addParameter("id", id)
-          .executeUpdate();
-        String sql2 = "DELETE FROM monsters WHERE player_id = :id";
-        con.createQuery(sql2)
-          .addParameter("id", id)
-          .executeUpdate();
-      }
+  public void delete() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "DELETE FROM players WHERE id = :id";
+      con.createQuery(sql)
+        .addParameter("id", id)
+        .executeUpdate();
+      String sql3 = "DELETE FROM cards_monsters_players where player_id = :id";
+      con.createQuery(sql3)
+        .addParameter("id", id)
+        .executeUpdate();
+      String sql2 = "DELETE FROM monsters WHERE player_id = :id";
+      con.createQuery(sql2)
+        .addParameter("id", id)
+        .executeUpdate();
     }
+  }
 
+  // Overrides
+  @Override
+  public boolean equals(Object otherPlayer){
+    if (!(otherPlayer instanceof Player))
+      return false;
+    Player newPlayer = (Player) otherPlayer;
+    return id == newPlayer.getId();
+  }
 
 }
