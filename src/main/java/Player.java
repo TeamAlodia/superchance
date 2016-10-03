b69840bd912aa73bd8e07826cd01fa3e8ac8f09e
Justin <justin.m.kincaid@gmail.com>
2016-10-03 10:19:48 -0700

    add basic database functions to player.java

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -37,4 +37,47 @@ public class Player {
     password = _password;
   }
 
+  // Database Functions
+  public void save() {
+      try(Connection con = DB.sql2o.open()) {
+        String sql = "INSERT INTO players (name, created, password) VALUES (:name, :created, :password)";
+        id = (int) con.createQuery(sql, true)
+          .addParameter("name", name)
+          .addParameter("created", created)
+          .addParameter("password", password)
+          .executeUpdate()
+          .getKey();
+      }
+    }
+
+    public void update() {
+      try(Connection con = DB.sql2o.open()) {
+        String sql = "UPDATE players SET name = :name, created = :created, password = :password WHERE id = :id";
+        con.createQuery(sql)
+          .addParameter("name", name)
+          .addParameter("created", created)
+          .addParameter("password", password)
+          .addParameter("id", id)
+          .executeUpdate();
+      }
+    }
+
+    public void delete() {
+      try(Connection con = DB.sql2o.open()) {
+        String sql = "DELETE FROM players WHERE id = :id";
+        con.createQuery(sql)
+          .addParameter("id", id)
+          .executeUpdate();
+        String sql3 = "DELETE FROM cards_monsters_players where player_id = :id";
+        con.createQuery(sql3)
+          .addParameter("id", id)
+          .executeUpdate();
+        String sql2 = "DELETE FROM monsters WHERE player_id = :id";
+        con.createQuery(sql2)
+          .addParameter("id", id)
+          .executeUpdate();
+      }
+    }
+
+
 }
