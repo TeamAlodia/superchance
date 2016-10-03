5b8a54fceb342db094e91cbd9caee67cec0cf28a
Zachary Matthewstearn <zammo911@gmail.com>
2016-10-03 16:34:19 -0700

    add CRUD for card class pending testing

--- src/main/java/Cards/Card.java
+++ src/main/java/Cards/Card.java
@@ -1,17 +1,27 @@
+import java.util.List;
+import org.sql2o.*;
+
 public abstract class Card{
   // Member Variables
   protected int id = 0;
-  protected int element_id = 0;
+  protected int elementId = 0;
   protected String name = CardName.NONE;
   protected String type = CardType.NONE;
   protected String description = "";
-  protected String location = CardLocation.DECK;
 
-  // Constructor
+  // Constructors
+  //// No element -- falls back to default of 0
   public Card(String _name, String _type, String _description){
     name = _name;
     type = _type;
     description = _description;
+  }
+  //// element
+  public Card(String _name, String _type, String _description, int _elementId){
+    name = _name;
+    type = _type;
+    description = _description;
+    elementId = _elementId;
   }
 
   // Getters
@@ -30,9 +40,6 @@ public abstract class Card{
   public String getDescription(){
     return description;
   }
-  public String getLocation(){
-    return location;
-  }
 
   // Setters
   public void setElementId(int _elementId){
@@ -47,24 +54,69 @@ public abstract class Card{
   public void setDescription(String _description){
     description = _description;
   }
-  public void setLocation(String _location){
-    location = _location;
+
+  // Overrides
+  @Override
+  public boolean equals(Object _other){
+    if(!(_other instanceof Card))
+      return false;
+    Card newCard = (Card) _other;
+    return id == newCard.id;
   }
 
   // CRUD
   //// Create
-  public void create(int _elementId){
+  public void create(){
     try(Connection con = DB.sql2o.open()){
-      String sql = "INSERT INTO cards (name, elementId, type, description, location) VALUES (:name, :elementId, :type, :description, :location) WHERE id = :id";
-      id = (int) con.createConnetion(sql,true)
-        .executeUpdate()
+      String sql = "INSERT INTO cards (name, element_id, card_type, description) VALUES (:name, :elementId, :type, :description)";
+      id = (int) con.createQuery(sql,true)
         .addParameter("name",name)
         .addParameter("elementId",elementId)
         .addParameter("type",type)
         .addParameter("description",description)
-        .addParameter("location",location)
-        .addParameter("id",id)
+        .executeUpdate()
         .getKey();
+    }
+  }
+
+  //// Read
+  public static List<Card> readAll(){
+    try(Connection con = DB.sql2o.open()){
+      String sql = "SELECT * FROM cards";
+      return con.createQuery(sql)
+        .executeAndFetch(Card.class);
+    }
+  }
+  public static Card readById(int _id){
+    try(Connection con = DB.sql2o.open()){
+      String sql = "SELECT * FROM cards WHERE id = :id";
+      return con.createQuery(sql)
+        .addParameter("id",_id)
+        .executeAndFetchFirst(Card.class);
+    }
+  }
+
+  //// Update
+  public void update(){
+    try(Connection con = DB.sql2o.open()){
+      String sql = "UPDATE cards SET element_id=:elementId, name=:name, card_type=:type, description=:description WHERE id = :id";
+      con.createQuery(sql)
+        .addParameter("elementId",elementId)
+        .addParameter("name",name)
+        .addParameter("type",type)
+        .addParameter("description",description)
+        .addParameter("id",id)
+        .executeAndFetchFirst(Card.class);
+    }
+  }
+
+  //// Delete
+  public void delete(){
+    try(Connection con = DB.sql2o.open()){
+      String sql = "DELETE FROM cards WHERE id = :id";
+      con.createQuery(sql)
+        .addParameter("id",id)
+        .executeUpdate();
     }
   }
 
--- src/test/java/Cards/AttackTest.java
+++ src/test/java/Cards/AttackTest.java
@@ -1,4 +1,5 @@
 import java.util.List;
+import java.util.ArrayList;
 import org.junit.*;
 import org.sql2o.*;
 import static org.junit.Assert.*;
@@ -17,4 +18,53 @@ public class AttackTest {
     assertEquals(true, testAttack instanceof Attack);
   }
 
+  //// Read All
+  @Test
+  public void readAll_returnsAllCardsFromDB_ListCard(){
+    Attack testAttack1 = new Attack();
+    Attack testAttack2 = new Attack();
+    testAttack1.create();
+    testAttack2.create();
+    List<Attack>expectedOutput = new ArrayList<>();
+    expectedOutput.add(testAttack1);
+    expectedOutput.add(testAttack2);
+    assertEquals(expectedOutput, Attack.readAll());
+  }
+
+  //// Equals
+  @Test
+  public void equals_returnsFalseAppropriately_false(){
+    Attack testAttack1 = new Attack();
+    Attack testAttack2 = new Attack();
+    testAttack1.create();
+    testAttack2.create();
+    assertEquals(false, testAttack1.equals(testAttack2));
+  }
+  @Test
+  public void equals_returnsTrueAppropriately_true(){
+    Attack testAttack1 = new Attack();
+    Attack testAttack2 = testAttack1;
+    testAttack1.create();
+    testAttack2.create();
+    assertEquals(true, testAttack1.equals(testAttack2));
+  }
+
+  //// Update
+  @Test
+  public void update_modifiesDBAppropriately_true(){
+    Attack testAttack = new Attack();
+    testAttack.create();
+    testAttack.setName("Test Name");
+    testAttack.update();
+    assertEquals(testAttack.getName(), Attack.readById(testAttack.getId()).getName());
+  }
+
+  //// Delete
+  @Test
+  public void delete_removesFromDBAppropriately_true(){
+    Attack testAttack = new Attack();
+    testAttack.create();
+    testAttack.delete();
+    assertEquals(0, Attack.readAll().size());
+  }
 }
