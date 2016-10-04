b17a15fd0b6219d06cb7e8475ea9c2cdeb65897d
Alaina Traxler <eloavox@gmail.com>
2016-10-03 21:16:08 -0700

    clean up Monster.java code, including implementing an interface, updating variables and functions for new database variables, full-state equals, updating testing and general bug-hunting.

--- src/main/java/Monster.java
+++ src/main/java/Monster.java
@@ -1,3 +1,14 @@
+// Replaced strength with power, updated all Function
+// Updated functions for new database structure
+// Set up constructor for species_id
+// Add getters for new variables
+// Organize methods
+// Set equals up for full-state comparison, immediately used it to find out where you were missing vriables in your save/update
+// Implemented DatabaseManagement
+
+
+// NOTE: Seperate your logic further. Don't call updates internally, and especially not inconsistently. It's just begging for someone to forget which method they have to manually update with.
+
 import java.util.List;
 import org.sql2o.*;
 import java.util.ArrayList;
@@ -6,9 +17,10 @@ import java.util.Date;
 import java.lang.Math;
 import java.util.Random;
 
-public class Monster {
+public class Monster implements DatabaseManagement{
   private int id;
   private int player_id;
+  private int species_id;
   private String name;
   private Timestamp born;
   private Timestamp last_interacted;
@@ -18,12 +30,14 @@ public class Monster {
   private boolean maxLevel = false;
   private int rest = 100;
   private int base_health = 100;
+  private int base_power;
+  private int base_defense;
   private int base_deck_size = 13;
   private int health = 10;
-  private int strength = 1;
+  private int power = 1;
   private int defense = 1;
   private int health_weight = 0;
-  private int strength_weight = 0;
+  private int power_weight = 0;
   private int defense_weight = 0;
   private String status;
 
@@ -34,7 +48,7 @@ public class Monster {
   public static final int EXPERIENCE_FOR_LEVEL = 100;
   public static final int POINTS_GAINED_PER_LEVEL = 10;
 
-  public Monster(int _player_id, String _name) {
+  public Monster(int _player_id, String _name, int _species_id) {
     player_id = _player_id;
     name = _name;
     born = new Timestamp(new Date().getTime());
@@ -47,6 +61,9 @@ public class Monster {
   }
   public int getPlayer_Id() {
     return player_id;
+  }
+  public int getSpecies_Id(){
+    return species_id;
   }
   public String getName() {
     return name;
@@ -72,14 +89,20 @@ public class Monster {
   public int getBase_Health() {
     return base_health;
   }
+  public int getBase_Power() {
+    return base_power;
+  }
+  public int getBase_Defense() {
+    return base_defense;
+  }
   public int getBase_Deck_Size() {
     return base_deck_size;
   }
   public int getHealth() {
     return health;
   }
-  public int getStrength() {
-    return strength;
+  public int getPower() {
+    return power;
   }
   public int getDefense() {
     return defense;
@@ -87,11 +110,14 @@ public class Monster {
   public int getHealth_Weight() {
     return health_weight;
   }
-  public int getStrength_Weight() {
-    return strength_weight;
+  public int getPower_Weight() {
+    return power_weight;
   }
   public int getDefense_Weight() {
     return defense_weight;
+  }
+  public String getStatus() {
+    return status;
   }
 
   public void setName(String _name) {
@@ -121,8 +147,8 @@ public class Monster {
   public void setHealth(int _health) {
     health = _health;
   }
-  public void setStrength(int _strength) {
-    strength = _strength;
+  public void setPower(int _power) {
+    power = _power;
   }
   public void setDefense(int _defense) {
     defense = _defense;
@@ -130,11 +156,125 @@ public class Monster {
   public void setHealth_Weight(int _health_weight) {
     health_weight = _health_weight;
   }
-  public void setStrength_Weight(int _strength_weight) {
-    strength_weight = _strength_weight;
+  public void setPower_Weight(int _power_weight) {
+    power_weight = _power_weight;
   }
   public void setDefense_Weight(int _defense_weight) {
     defense_weight = _defense_weight;
+  }
+
+  // Database Functions
+  @Override
+  public boolean equals(Object otherMonster){
+    if (!(otherMonster instanceof Monster))
+      return false;
+    Monster newMonster = (Monster) otherMonster;
+    return id == newMonster.getId() &&
+      species_id == newMonster.getSpecies_Id() &&
+      name.equals(newMonster.getName()) &&
+      born.equals(newMonster.getBorn()) &&
+      last_interacted.equals(newMonster.getLast_Interacted()) &&
+      in_battle == newMonster.getIn_Battle() &&
+      exp == newMonster.getExp() &&
+      level == newMonster.getLevel() &&
+      rest == newMonster.getRest() &&
+      base_health == newMonster.getBase_Health() &&
+      base_power == newMonster.getBase_Power() &&
+      base_defense == newMonster.getBase_Defense() &&
+      base_deck_size == newMonster.getBase_Deck_Size() &&
+      power == newMonster.getPower() &&
+      defense == newMonster.getDefense() &&
+      health_weight == newMonster.getHealth_Weight() &&
+      power_weight == newMonster.getPower_Weight() &&
+      defense_weight == newMonster.getPower_Weight();// &&
+      // status.equals(newMonster.getStatus());
+  }
+
+  public void save() {
+      try(Connection con = DB.sql2o.open()) {
+        String sql = "INSERT INTO monsters (player_id, species_id, name, born, last_interacted, in_battle, exp, level, rest, base_health, base_power, base_defense, base_deck_size, health, power, defense, health_weight, power_weight, defense_weight, status) VALUES (:player_id, :species_id, :name, :born, :last_interacted, :in_battle, :exp, :level, :rest, :base_health, :base_power, :base_defense, :base_deck_size, :health, :power, :defense, :health_weight, :power_weight, :defense_weight, :status)";
+        id = (int) con.createQuery(sql, true)
+          .addParameter("player_id", player_id)
+          .addParameter("species_id", species_id)
+          .addParameter("name", name)
+          .addParameter("born", born)
+          .addParameter("last_interacted", last_interacted)
+          .addParameter("in_battle", in_battle)
+          .addParameter("exp", exp)
+          .addParameter("level", level)
+          .addParameter("rest", rest)
+          .addParameter("base_health", base_health)
+          .addParameter("base_power", base_power)
+          .addParameter("base_defense", base_defense)
+          .addParameter("base_deck_size", base_deck_size)
+          .addParameter("health", health)
+          .addParameter("power", power)
+          .addParameter("defense", defense)
+          .addParameter("health_weight", health_weight)
+          .addParameter("power_weight", power_weight)
+          .addParameter("defense_weight", defense_weight)
+          .addParameter("status", status)
+          .executeUpdate()
+          .getKey();
+      }
+    }
+
+  public void update() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "UPDATE monsters SET player_id = :player_id, species_id = :species_id, name = :name, born = :born, last_interacted = :last_interacted, in_battle = :in_battle, exp = :exp, level = :level, rest = :rest, base_health = :base_health, base_power = :base_power, base_defense = :base_defense, base_deck_size = :base_deck_size, health = :health, power = :power, defense = :defense, health_weight = :health_weight, power_weight = :power_weight, defense_weight = :defense_weight, status = :status WHERE id = :id";
+
+      con.createQuery(sql)
+        .addParameter("player_id", player_id)
+        .addParameter("species_id", species_id)
+        .addParameter("name", name)
+        .addParameter("born", born)
+        .addParameter("last_interacted", last_interacted)
+        .addParameter("in_battle", in_battle)
+        .addParameter("exp", exp)
+        .addParameter("level", level)
+        .addParameter("rest", rest)
+        .addParameter("base_health", base_health)
+        .addParameter("base_power", base_power)
+        .addParameter("base_defense", base_defense)
+        .addParameter("base_deck_size", base_deck_size)
+        .addParameter("health", health)
+        .addParameter("power", power)
+        .addParameter("defense", defense)
+        .addParameter("health_weight", health_weight)
+        .addParameter("power_weight", power_weight)
+        .addParameter("defense_weight", defense_weight)
+        .addParameter("status", status)
+        .addParameter("id", id)
+        .executeUpdate();
+    }
+  }
+
+  public void delete() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "DELETE FROM monsters WHERE id = :id";
+      con.createQuery(sql)
+        .addParameter("id", id)
+        .executeUpdate();
+    }
+  }
+
+  public static List<Monster> all() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM monsters";
+      List<Monster> allMonsters = con.createQuery(sql)
+      .executeAndFetch(Monster.class);
+      return allMonsters;
+    }
+  }
+
+  public static Monster find(int _id){
+    try(Connection con = DB.sql2o.open()){
+      String sql = "SELECT * FROM monsters WHERE id=:id";
+
+      return con.createQuery(sql)
+        .addParameter("id", _id)
+        .executeAndFetchFirst(Monster.class);
+    }
   }
 
   // Increment Functions
@@ -163,16 +303,16 @@ public class Monster {
     }
 
     int totalGain = 0;
-    int weightTotal = health_weight + defense_weight + strength_weight;
+    int weightTotal = health_weight + defense_weight + power_weight;
     int healthGain = 0;
-    int strengthGain = 0;
+    int powerGain = 0;
     int defenseGain = 0;
 
     if(weightTotal > 0) {
       healthGain = (int)((health_weight / weightTotal) * POINTS_GAINED_PER_LEVEL * 10);
-      strengthGain = (int)((strength_weight / weightTotal) * POINTS_GAINED_PER_LEVEL);
+      powerGain = (int)((power_weight / weightTotal) * POINTS_GAINED_PER_LEVEL);
       defenseGain = (int)((defense_weight / weightTotal) * POINTS_GAINED_PER_LEVEL);
-      totalGain = healthGain/10 + strengthGain + defenseGain;
+      totalGain = healthGain/10 + powerGain + defenseGain;
     }
 
 
@@ -182,7 +322,7 @@ public class Monster {
       if(number == 1) {
         healthGain += 10;
       } else if (number == 2) {
-        strengthGain += 1;
+        powerGain += 1;
       } else {
         defenseGain += 1;
       }
@@ -191,12 +331,12 @@ public class Monster {
 
     base_health += healthGain;
     health += healthGain;
-    strength += strengthGain;
+    power += powerGain;
     defense += defenseGain;
     base_deck_size += 3;
     health_weight = 0;
     defense_weight = 0;
-    strength_weight = 0;
+    power_weight = 0;
 
     level += _gain;
   }
@@ -230,8 +370,8 @@ public class Monster {
     return number;
   }
 
-  public void trainStrength() {
-    strength_weight += this.train();
+  public void trainPower() {
+    power_weight += this.train();
     this.update();
   }
 
@@ -243,96 +383,6 @@ public class Monster {
   public void trainHealth() {
     health_weight += this.train();
     this.update();
-  }
-
-  // Find Functions
-  public static List<Monster> all() {
-    try(Connection con = DB.sql2o.open()) {
-      String sql = "SELECT * FROM monsters";
-      List<Monster> allMonsters = con.createQuery(sql)
-      .executeAndFetch(Monster.class);
-      return allMonsters;
-    }
-  }
-
-  public static Monster find(int _id){
-    try(Connection con = DB.sql2o.open()){
-      String sql = "SELECT * FROM monsters WHERE id=:id";
-
-      return con.createQuery(sql)
-        .addParameter("id", _id)
-        .executeAndFetchFirst(Monster.class);
-    }
-  }
-
-  // Database Functions
-  public void save() {
-      try(Connection con = DB.sql2o.open()) {
-        String sql = "INSERT INTO monsters (player_id, name, born, last_interacted, in_battle, exp, level, rest, base_health, base_deck_size, health, strength, defense, health_weight, strength_weight, defense_weight) VALUES (:player_id, :name, :born, :last_interacted, :in_battle, :exp, :level, :rest, :base_health, :base_deck_size, :health, :strength, :defense, :health_weight, :strength_weight, :defense_weight)";
-        id = (int) con.createQuery(sql, true)
-          .addParameter("player_id", player_id)
-          .addParameter("name", name)
-          .addParameter("born", born)
-          .addParameter("last_interacted", last_interacted)
-          .addParameter("in_battle", in_battle)
-          .addParameter("exp", exp)
-          .addParameter("level", level)
-          .addParameter("rest", rest)
-          .addParameter("base_health", base_health)
-          .addParameter("base_deck_size", base_deck_size)
-          .addParameter("health", health)
-          .addParameter("strength", strength)
-          .addParameter("defense", defense)
-          .addParameter("health_weight", health_weight)
-          .addParameter("strength_weight", strength_weight)
-          .addParameter("defense_weight", defense_weight)
-          .executeUpdate()
-          .getKey();
-      }
-    }
-
-  public void update() {
-    try(Connection con = DB.sql2o.open()) {
-      String sql = "UPDATE monsters SET player_id = :player_id, name = :name, born = :born, last_interacted = :last_interacted, in_battle = :in_battle, exp = :exp, level = :level, rest = :rest, base_health = :base_health, base_deck_size = :base_deck_size, health = :health, strength = :strength, defense = :defense, health_weight = :health_weight, strength_weight = :strength_weight, defense_weight = :defense_weight WHERE id = :id";
-
-      con.createQuery(sql)
-        .addParameter("player_id", player_id)
-        .addParameter("name", name)
-        .addParameter("born", born)
-        .addParameter("last_interacted", last_interacted)
-        .addParameter("in_battle", in_battle)
-        .addParameter("exp", exp)
-        .addParameter("level", level)
-        .addParameter("rest", rest)
-        .addParameter("base_health", base_health)
-        .addParameter("base_deck_size", base_deck_size)
-        .addParameter("health", health)
-        .addParameter("strength", strength)
-        .addParameter("defense", defense)
-        .addParameter("health_weight", health_weight)
-        .addParameter("strength_weight", strength_weight)
-        .addParameter("defense_weight", defense_weight)
-        .addParameter("id", id)
-        .executeUpdate();
-    }
-  }
-
-  public void delete() {
-    try(Connection con = DB.sql2o.open()) {
-      String sql = "DELETE FROM monsters WHERE id = :id";
-      con.createQuery(sql)
-        .addParameter("id", id)
-        .executeUpdate();
-    }
-  }
-
-  // Overrides
-  @Override
-  public boolean equals(Object otherMonster){
-    if (!(otherMonster instanceof Monster))
-      return false;
-    Monster newMonster = (Monster) otherMonster;
-    return id == newMonster.getId();
   }
 
 }
--- src/test/java/MonsterTest.java
+++ src/test/java/MonsterTest.java
@@ -1,3 +1,5 @@
+// Updated all tests for power variables and new constructor
+
 import org.sql2o.*;
 import org.junit.*;
 import static org.junit.Assert.*;
@@ -15,27 +17,27 @@ public class MonsterTest {
   // Constructor Tests
   @Test
   public void monster_instantiatesNewMonster() {
-    Monster firstMonster = new Monster(1, "Rompy");
+    Monster firstMonster = new Monster(1, "Rompy", 1);
     assertTrue(firstMonster instanceof Monster);
   }
 
   // Getter Tests
   @Test
   public void getName_monsterInstantiatesWithName_Rompy() {
-    Monster firstMonster = new Monster(1, "Rompy");
+    Monster firstMonster = new Monster(1, "Rompy", 1);
     assertEquals("Rompy", firstMonster.getName());
   }
 
   @Test
   public void getBorn_monsterInstantiatesWithTimeCreated_Timestamp() {
-    Monster firstMonster = new Monster(1, "Rompy");
+    Monster firstMonster = new Monster(1, "Rompy", 1);
     int verifyTime = new Timestamp(new Date().getTime()).getDay();
     assertEquals(verifyTime, firstMonster.getBorn().getDay());
   }
 
   @Test
   public void getLastInteracted_monsterInstantiatesWithLastInteracted_Timestamp() {
-    Monster firstMonster = new Monster(1, "Rompy");
+    Monster firstMonster = new Monster(1, "Rompy", 1);
     int verifyTime = new Timestamp(new Date().getTime()).getDay();
     assertEquals(verifyTime, firstMonster.getLast_Interacted().getDay());
   }
@@ -43,14 +45,14 @@ public class MonsterTest {
   // Experience Tests
   @Test
   public void incrementexp_monsterIncreasesExp() {
-    Monster firstMonster = new Monster(1, "Rompy");
+    Monster firstMonster = new Monster(1, "Rompy", 1);
     firstMonster.incrementExp(3);
     assertEquals(3, firstMonster.getExp());
   }
 
   @Test
   public void incrementexp_monsterexpResetsOnLevel() {
-    Monster firstMonster = new Monster(1, "Rompy");
+    Monster firstMonster = new Monster(1, "Rompy", 1);
     firstMonster.incrementExp(101);
     assertEquals(1, firstMonster.getExp());
     assertEquals(2, firstMonster.getLevel());
@@ -58,7 +60,7 @@ public class MonsterTest {
 
   @Test
   public void incrementLevel_monsterLevelIncreasesCorrectly() {
-    Monster firstMonster = new Monster(1, "Rompy");
+    Monster firstMonster = new Monster(1, "Rompy", 1);
     firstMonster.incrementLevel(3);
     assertEquals(4, firstMonster.getLevel());
   }
@@ -67,7 +69,7 @@ public class MonsterTest {
   // Rest Tests
   @Test
   public void incrementRest_monsterRestStaysBelow101() {
-    Monster firstMonster = new Monster(1, "Rompy");
+    Monster firstMonster = new Monster(1, "Rompy", 1);
     try {
       firstMonster.incrementRest(10);
     } catch (UnsupportedOperationException exception) {}
@@ -76,7 +78,7 @@ public class MonsterTest {
 
   @Test
   public void incrementRest_monsterRestIncreasesCorrectly() {
-    Monster firstMonster = new Monster(1, "Rompy");
+    Monster firstMonster = new Monster(1, "Rompy", 1);
     firstMonster.decrementRest(20);
     firstMonster.incrementRest(10);
     assertEquals(90, firstMonster.getRest());
@@ -84,7 +86,7 @@ public class MonsterTest {
 
   @Test
   public void decrementRest_monsterRestDecrementsCorrectly() {
-    Monster firstMonster = new Monster(1, "Rompy");
+    Monster firstMonster = new Monster(1, "Rompy", 1);
     firstMonster.decrementRest(20);
     assertEquals(80, firstMonster.getRest());
   }
@@ -92,7 +94,7 @@ public class MonsterTest {
   // Training Tests
   @Test
   public void train_returnsNumber() {
-    Monster firstMonster = new Monster(1, "Rompy");
+    Monster firstMonster = new Monster(1, "Rompy", 1);
     int test = firstMonster.train();
     boolean check = false;
     if(test < 11 && test > 0) {
@@ -102,29 +104,29 @@ public class MonsterTest {
   }
 
   @Test
-  public void trainStrength_increasesStrengthWeight() {
-    Monster firstMonster = new Monster(1, "Rompy");
-    firstMonster.trainStrength();
-    assertTrue(firstMonster.getStrength_Weight() > 0);
+  public void trainPower_increasesPowerWeight() {
+    Monster firstMonster = new Monster(1, "Rompy", 1);
+    firstMonster.trainPower();
+    assertTrue(firstMonster.getPower_Weight() > 0);
   }
 
   @Test
   public void trainDefense_increasesDefenseWeight() {
-    Monster firstMonster = new Monster(1, "Rompy");
+    Monster firstMonster = new Monster(1, "Rompy", 1);
     firstMonster.trainDefense();
     assertTrue(firstMonster.getDefense_Weight() > 0);
   }
 
   @Test
   public void trainHealth_increasesHealthWeight() {
-    Monster firstMonster = new Monster(1, "Rompy");
+    Monster firstMonster = new Monster(1, "Rompy", 1);
     firstMonster.trainHealth();
     assertTrue(firstMonster.getHealth_Weight() > 0);
   }
 
   @Test
   public void train_decreasesRest() {
-    Monster firstMonster = new Monster(1, "Rompy");
+    Monster firstMonster = new Monster(1, "Rompy", 1);
     firstMonster.trainHealth();
     assertEquals(firstMonster.getRest(), 80);
   }
@@ -132,9 +134,9 @@ public class MonsterTest {
   // Find Tests
   @Test
   public void all_returnsAllInstancesOfMonster_true() {
-    Monster firstMonster = new Monster(1, "Rompy");
+    Monster firstMonster = new Monster(1, "Rompy", 1);
     firstMonster.save();
-    Monster secondMonster = new Monster(2, "Sniffles");
+    Monster secondMonster = new Monster(2, "Sniffles", 2);
     secondMonster.save();
     assertEquals(true, Monster.all().get(0).equals(firstMonster));
     assertEquals(true, Monster.all().get(1).equals(secondMonster));
@@ -143,14 +145,14 @@ public class MonsterTest {
   // Database Tests
   @Test
   public void save_insertsObjectIntoDatabase_Monster() {
-    Monster firstMonster = new Monster(1, "Rompy");
+    Monster firstMonster = new Monster(1, "Rompy", 1);
     firstMonster.save();
     assertEquals(true, Monster.all().get(0).equals(firstMonster));
   }
 
   @Test
   public void save_assignsIdToObject() {
-    Monster firstMonster = new Monster(1, "Rompy");
+    Monster firstMonster = new Monster(1, "Rompy", 1);
     firstMonster.save();
     Monster savedMonster = Monster.all().get(0);
     assertEquals(firstMonster.getId(), savedMonster.getId());
@@ -158,7 +160,7 @@ public class MonsterTest {
 
   @Test
   public void delete_deletesMonster_true() {
-    Monster firstMonster = new Monster(1, "Rompy");
+    Monster firstMonster = new Monster(1, "Rompy", 1);
     firstMonster.save();
     firstMonster.delete();
     assertEquals(0, Monster.all().size());
@@ -167,8 +169,8 @@ public class MonsterTest {
   // Override Tests
   @Test
   public void equals_returnsTrueIfNameAndEmailAreSame_true() {
-    Monster firstMonster = new Monster(1, "Rompy");
-    Monster testMonster = new Monster(1, "Rompy");
+    Monster firstMonster = new Monster(1, "Rompy", 1);
+    Monster testMonster = new Monster(1, "Rompy", 1);
     assertTrue(testMonster.equals(firstMonster));
   }
 
