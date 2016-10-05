04d50e4baa6246ba8cd5690a6a86d35e943c80a2
Alaina Traxler <eloavox@gmail.com>
2016-10-05 06:23:49 -0700

    add database functions to Player.java

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -42,7 +42,73 @@ public class Player {
     losses++;
   }
 
+  @Override
+  public boolean equals(Object _otherObject){
+    if(!(_otherObject instanceof Player)){
+      return false;
+    }else{
+      Player otherObject = (Player) _otherObject;
+      return id == otherObject.getId() &&
+        name.equals(otherObject.getName()) &&
+        wins == otherObject.getWins() &&
+        losses == otherObject.getLosses();
+    }
+  }
 
+  public void save(){
+    try(Connection con = DB.sql2o.open()){
+      String sql = "INSERT INTO players (name, wins, losses) VALUES (:name, :wins, :losses)";
+
+      id = (int) con.createQuery(sql, true)
+        .addParameter("name", name)
+        .addParameter("wins", wins)
+        .addParameter("losses", losses)
+        .executeUpdate()
+        .getKey();
+    }
+  }
+
+  public static Player find(int _id){
+    try(Connection con = DB.sql2o.open()){
+      String sql = "SELECT * FROM players WHERE id=:id";
+      return con.createQuery(sql)
+        .addParameter("id", _id)
+        .executeAndFetchFirst(Player.class);
+    }
+  }
+
+  public static List<Player> all(){
+    try(Connection con = DB.sql2o.open()){
+      String sql = "SELECT * FROM players";
+
+      return con.createQuery(sql)
+        .executeAndFetch(Player.class);
+    }
+
+  }
+
+  public void update(){
+    try(Connection con = DB.sql2o.open()){
+      String sql = "UPDATE players SET name = :name, wins = :wins, losses = :losses WHERE id=:id";
+
+      con.createQuery(sql)
+        .addParameter("name", name)
+        .addParameter("wins", wins)
+        .addParameter("losses", losses)
+        .addParameter("id", id)
+        .executeUpdate();
+    }
+  }
+
+  public void delete(){
+    try(Connection con = DB.sql2o.open()){
+      String sql = "DELETE FROM players WHERE id=:id";
+
+      con.createQuery(sql)
+        .addParameter("id", id)
+        .executeUpdate();
+    }
+  }
 
   // incrementWins
   // increases wins by one
@@ -50,7 +116,7 @@ public class Player {
   // incrementLosses
   // increses losses by one
 
-  // addMonster
+  // addPlayer
   // takes in monster_id
-  // adds entry into players_monsters
+  // adds entry into players_players
 }
