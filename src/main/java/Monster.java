395f0a070d52f3ea5cc20d4f2673f3ff029e75ad
Alaina Traxler <eloavox@gmail.com>
2016-10-05 05:21:40 -0700

    add getters/setters for Monster.java

--- src/main/java/Monster.java
+++ src/main/java/Monster.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 public class Monster {
   // Database vars
   private int id;
+  private int player_id;
   private int species_id;
   private String species_name;
   private String name;
@@ -22,6 +23,114 @@ public class Monster {
   private boolean alive = true;
   private String status;
 
+  public Monster(int _species_id, String _name){
+    species_id = _species_id;
+    name = _name;
+
+    // Instantiate a species of the right type and populate remaining vars
+  }
+
+
+  // Getters/Setters
+  public int getId(){
+    return id;
+  }
+
+  public int getPlayer_Id(){
+    return player_id;
+  }
+
+  public int getSpecies_Id(){
+    return species_id;
+  }
+
+  public String getSpecies_Name(){
+    return species_name;
+  }
+
+  public String getName(){
+    return name;
+  }
+
+  public int getMax_Health(){
+    return max_health;
+  }
+
+  public int getBase_Power(){
+    return base_power;
+  }
+
+  public int getBase_Defense(){
+    return base_defense;
+  }
+
+  public int getWins(){
+    return wins;
+  }
+
+  public int getLosses(){
+    return losses;
+  }
+
+  public int getHealth(){
+    return health;
+  }
+
+  public int getPower(){
+    return power;
+  }
+
+  public int getDefense(){
+    return defense;
+  }
+
+  public boolean getAdvantage(){
+    return advantage;
+  }
+
+  public boolean getAlive(){
+    return alive;
+  }
+
+  public String getStatus(){
+    return status;
+  }
+
+  public void setName(String _name){
+    name = _name;
+  }
+
+  public void incrementWins(){
+    wins++;
+  }
+
+  public void incrementLosses(){
+    losses++;
+  }
+
+  public void setHealth(int _health){
+    health = _health;
+  }
+
+  public void setPower(int _power){
+    power = _power;
+  }
+
+  public void setDefense(int _defense){
+    defense = _defense;
+  }
+
+  public void setAdvantage(boolean _advantage){
+    advantage = _advantage;
+  }
+
+  public void setAlive(boolean _alive){
+    alive = _alive;
+  }
+
+  public void setStatus(String _status){
+    status = _status;
+  }
 
   // increaseHealth
   // receives int
