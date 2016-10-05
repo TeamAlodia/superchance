d8c6b61d7a6c856fda21726d85ad54e9cef45a2b
Tony Fuentes <leobender@gmail.com>
2016-10-04 17:15:20 -0700

    added complex combat methods

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,12 +9,23 @@ public class App {
   public static void main(String[] args) {
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
+    ProcessBuilder process = new ProcessBuilder();
+     Integer port;
+     if (process.environment().get("PORT") != null) {
+         port = Integer.parseInt(process.environment().get("PORT"));
+     } else {
+         port = 4567;
+     }
+
+    setPort(port);
 
     get("/test",(request,response)->{
       Map<String,Object> model = new HashMap<>();
 
-      OrcDeckCreator testDeck = new OrcDeckCreator();
+      // OrcDeckCreator testDeck = new OrcDeckCreator();
+      // DeckCreator realDeck = new DeckCreator();
 
+      model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     },new VelocityTemplateEngine());
 
@@ -31,6 +42,17 @@ public class App {
       model.put("allSpecies", Species.all());
       model.put("monsters", Monster.all());
       model.put("template", "templates/monsters.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/monsters/:id", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      int monster_id = Integer.parseInt(request.params(":id"));
+      Monster monster = Monster.find(monster_id);
+
+      model.put("player", Player.find(monster.getPlayer_Id()));
+      model.put("monster", monster);
+      model.put("template", "templates/monster.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
@@ -51,6 +73,22 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    get("/species", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+
+      model.put("allSpecies", Species.all());
+      model.put("template", "templates/allspecies.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/species/:id", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      int species_id = Integer.parseInt(request.params(":id"));
+
+      model.put("species", Species.find(species_id));
+      model.put("template", "templates/species.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
     //-------------------------------------------------------
 
     post("/players", (request, response) -> {
@@ -69,6 +107,19 @@ public class App {
         response.redirect("/players");
         return "Player not created";
       }
+    });
+
+    post("/monsters", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      int player_id = Integer.parseInt(request.queryParams("player_id"));
+      int species_id = Integer.parseInt(request.queryParams("species_id"));
+      String name = request.queryParams("name");
+
+      Monster monster = new Monster(player_id, species_id, name);
+      monster.save();
+
+      response.redirect("/monsters");
+      return "Monster created";
     });
   }
 }
--- src/main/java/Combat.java
+++ src/main/java/Combat.java
@@ -1,9 +1,48 @@
 import java.util.List;
 import org.sql2o.*;
 import java.util.ArrayList;
+import java.util.Random;
 
-/* Current Assumptions of how combat will work
+/* Combat pseudo code
 
+// Pre Combat Work
+1. Update IN_BATTLE = true for both monsters
+2. Calculate HEALTH = base_health(REST/100)
+3. POWER = base_power && DEFENSE = base_defense
+4. Calculate Monster's DECK size based on Current Rest value
+5  Retrieve all the card_ids for each monster and populate them into card_monsters as INACTIVE
+6. Let players select their DECKs from card_monsters, mark as DECK
+7. Select 5 cards at random from DECK and put in HAND
+
+
+// Players Select Cards
+For each Player:
+1. If DECK size == 0, shuffle INACTIVE hards into DECK
+2. Select card from DECK and put in HAND
+3. Player selects card from HAND and mark XXX_monster_done = true
+
+// Combat Logic (if (first_monster_done == true && second_monster_done = true)
+1. Select ABILITY from cards_abilities where cards_abilities.card_id == cards.id
+2. ABILITY.action(cards_abilities.ability_id, card.targetId(), cards_abilities.value);
+[update target monster](depends on how we want to seperate concerns)
+3. ???
+4. Update HEALTH for both monsters
+5. Update card state to DISCARD
+
+// Post Combat Work (if first_monster.HEALTH == 0 || second_monster.HEALTH ==0)
+1. Determine Winner and Loser
+2. Update Monsters:
+  a. Winner gets 100% XP
+  b. Loser gets 25% XP
+  c. Both get -50% REST
+  d. Both get LAST_INTERACTED update
+  e. Update IN_BATTLE = false for both
+3. Remove all cards from CARDS_MONSTERS
+4. Trigger Winner/Loser UI
+
+// ???
+- Where to put the combat loop? App.java
+- Case where both DEFEND?
 
 */
 public class Combat {
@@ -14,6 +53,10 @@ public class Combat {
   private int second_monster_id;
   private boolean first_monster_done;
   private boolean second_monster_done;
+
+  // Class Variables
+  int first_monster_deck_size;
+  int second_monster_deck_size;
 
   public Combat(int _first_monster_id, int _second_monster_id){
     first_monster_id = _first_monster_id;
@@ -43,13 +86,6 @@ public class Combat {
 
   public boolean getSecondMonsterDone() {
     return second_monster_done;
-  }
-
-  public void addCard(int _monster_id, int _card_id) {
-    // if(_monster_id == first_monster_id)
-    //   first_monster_cards.add(Card.find(_card_id));
-    // else
-    //   second_monster_cards.add(Card.find(_card_id));
   }
 
   // Database Methods
@@ -114,19 +150,142 @@ public class Combat {
     }
   }
 
+//----------------- Custom Combat Methods -------------
 
-  // Combat Methods
-  public void attack(int _attacking_monster_id){
-    Monster attacking_monster = Monster.find(_attacking_monster_id);
-    // private Monster defending_monster;
-    // if(first_monster_id != _attacking_monster_id)
-    //   defending_monster = Monster.find(first_monster_id);
-    // else
-    //   defending_monster = Monster.find(second_monster_id);
+  public void initializeCombat(int _monster_id){
+
+    deleteMonsterCards(_monster_id);
+
+    Monster monster = Monster.find(_monster_id);
+
+    int battleHealth = monster.getBase_Health() * (1/monster.getRest());
+
+    monster.setIn_Battle(false);
+    monster.setHealth(battleHealth);
+    monster.setPower(monster.getBase_Power());
+    monster.setDefense(monster.getBase_Defense());
+    monster.save();
+
+    List<Integer> monsterCards = getAllPotentialCardIds(_monster_id);
+
+    // add all cards as inactive
+    for(Integer card_id : monsterCards){
+	     addCardToDeck(card_id, _monster_id);
+    }
+
   }
 
-  public void defend(){
-
+  public List<Integer> getAllPotentialCardIds(int _monster_id){
+    Monster monster = Monster.find(_monster_id);
+    try(Connection con = DB.sql2o.open()){
+      String sql = "SELECT card_id FROM species_potential_cards WHERE species_id=:species_id";
+      return con.createQuery(sql)
+      .addParameter("species_id", monster.getSpecies_Id())
+      .executeAndFetch(Integer.class);
+    }
   }
+
+  public int getDeckSize(int _monster_id){
+    Monster monster = Monster.find(_monster_id);
+
+    int deck_size = monster.getBase_Deck_Size() * (1/monster.getRest());
+
+    return deck_size;
+  }
+
+  public void addCardToDeck (int _card_id, int _monster_id){
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO cards_monsters (card_id, monster_id, state) VALUES (:card_id, :monster_id, :state)";
+      con.createQuery(sql, true)
+        .addParameter("card_id", _card_id)
+        .addParameter("monster_id", _monster_id)
+        .addParameter("state", CardStatus.DECK)
+        .executeUpdate();
+    }
+  }
+
+  public void updateMonsterCard(int _card_monsters_id , String _card_type){
+    try(Connection con = DB.sql2o.open()){
+      String sql = "UPDATE cards_monsters SET state=:state WHERE id=:id";
+      con.createQuery(sql)
+        .addParameter("state", _card_type)
+        .addParameter("id", _card_monsters_id)
+        .executeUpdate();
+    }
+  }
+
+  public void getRandomDeckCard(int _monster_id) {
+
+    List<Integer> deckCards = getMonsterCards(_monster_id, CardStatus.DECK);
+
+    //Reshuffle deck if depleated
+    if(deckCards.isEmpty()) {
+      deckCards = getMonsterCards(_monster_id, CardStatus.DISCARD);
+
+      for(Integer card_id : deckCards)
+         updateMonsterCard(card_id, CardStatus.DECK);
+
+      deckCards = getMonsterCards(_monster_id, CardStatus.DECK);
+    }
+
+    //Select random card and put it in the hand
+    int card_index = deckCards.get(randomInt(0, deckCards.size()-1));
+    updateMonsterCard(card_index, CardStatus.HAND);
+
+    //return Card.readById(5);
+  }
+
+  //  Marks card discarded and returns the card_id
+  public int playCard(int _card_monsters_id){
+    try(Connection con = DB.sql2o.open()){
+
+      String sql = "SELECT card_id FROM cards_monsters WHERE id=:id";
+      int card_id = con.createQuery(sql)
+        .addParameter("id", _card_monsters_id)
+        .executeAndFetchFirst(Integer.class);
+
+      sql = "UPDATE cards_monsters SET state=:state WHERE id=:id";
+      con.createQuery(sql)
+        .addParameter("state", CardStatus.DISCARD)
+        .addParameter("id", _card_monsters_id)
+        .executeUpdate();
+
+      return card_id;
+    }
+  }
+
+  // returns a list of all of the card_ids for cards in the HAND
+  public List<Integer> getMonsterCards(int _monster_id, String _card_type){
+    try(Connection con = DB.sql2o.open()){
+      String sql = "SELECT id FROM cards_monsters WHERE monster_id=:monster_id AND state=:state";
+      return con.createQuery(sql)
+      .addParameter("monster_id", _monster_id)
+      .addParameter("state", _card_type)
+      .executeAndFetch(Integer.class);
+    }
+  }
+
+  private int randomInt(int min, int max){
+    Random rand = new Random();
+    return rand.nextInt(max - min)+min;
+  }
+
+  public void deleteMonsterCards(int _monster_id){
+    try(Connection con = DB.sql2o.open()){
+      String sql = "DELETE FROM cards_monsters WHERE monster_id = :monster_id";
+      con.createQuery(sql)
+        .addParameter("monster_id", _monster_id)
+        .executeUpdate();
+    }
+  }
+
+
+
+
+
+
+
+
+
 
 }
