e3a1f51e978ca902a2726aefc8c111faca82565e
Zachary Matthewstearn <zammo911@gmail.com>
2016-10-06 11:18:22 -0700

    added howto page

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -18,6 +18,12 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    get("/howto", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/howto.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
     get("/players", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
 
